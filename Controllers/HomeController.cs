ecab9dd20c2f0984e940e590a3f3b5e18df102c7
tyanfarm <phamquangtuyen.nt@gmail.com>
2024-03-26 14:23:38 +0700

    Contact & About cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
