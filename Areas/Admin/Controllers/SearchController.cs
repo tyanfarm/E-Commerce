87afb19d1e4d53a5f35213147b580e707ae7d117
tyanfarm <phamquangtuyen.nt@gmail.com>
2024-03-30 15:15:38 +0700

    add ProductSearch method

---
+++ Areas/Admin/Controllers/SearchController.cs
@@ -0,0 +1,38 @@
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Areas.Admin.Controllers {
+    [Area("Admin")]
+    public class SearchController : Controller {
+        private readonly EcommerceContext _context;
+
+        public SearchController(EcommerceContext context) {
+            _context = context;
+        }
+
+        // POST: Admin/Search/FindProduct
+        [HttpPost]
+        public IActionResult FindProduct(string keyword) {
+            List<Product> listProducts = new List<Product>();
+
+            if (string.IsNullOrEmpty(keyword) || keyword.Length < 1) {
+                return PartialView("ListProductsSearchPartial", null);
+            }
+
+            // Search product
+            listProducts = _context.Products.AsNoTracking()
+                                            .Include(p => p.Cat)
+                                            .Where(p => p.ProductName.Contains(keyword))
+                                            .OrderByDescending(p => p.ProductName)
+                                            .ToList();
+
+            if (listProducts == null) {
+                return PartialView("ListProductsSearchPartial", null);
+            }
+            else {
+                return PartialView("ListProductsSearchPartial", listProducts);
+            }
+        }
+    }
+}
