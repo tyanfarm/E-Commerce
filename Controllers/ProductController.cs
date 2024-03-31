825d96d8c4074c66550832693ec5559fcba80eca
tyanfarm <phamquangtuyen.nt@gmail.com>
2024-03-31 19:06:06 +0700

    Product View & ProductDetails View

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -2,39 +2,70 @@ using AspNetCoreHero.ToastNotification.Abstractions;
 using E_Commerce.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using PagedList.Core;
 
 namespace E_Commerce.Controllers {
     public class ProductController : Controller {
         private readonly EcommerceContext _context;
-        public INotyfService _notyfService {get; }
 
-        public ProductController(EcommerceContext context, INotyfService notyfService) {
+        public ProductController(EcommerceContext context) {
             _context = context;
-            _notyfService = notyfService;
         }
 
-        public IActionResult Index() 
+        public IActionResult Index(int? page) 
         {
-            return View();
+            try {
+                // Pagination
+                var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+                var pageSize = 10;
+                var listProducts = _context.Products.AsNoTracking()
+                                                    .OrderByDescending(p => p.ProductId);
+
+                PagedList<Product> models = new PagedList<Product>(listProducts, pageNumber, pageSize);
+                ViewBag.CurrentPage = pageNumber;
+
+                return View(models);
+            }
+            catch {
+                return RedirectToAction("Index", "Home");
+            }
         }
 
-        public IActionResult Details() 
-        {
-            return View();
+        [Route("/product-{id}.html", Name="productDetails")] 
+        public async Task<IActionResult> Details(int id) {
+            try {
+                var product = await _context.Products.FirstOrDefaultAsync(p => p.ProductId == id);
+
+                if (product == null) {
+                    return RedirectToAction("Index");
+                }
+
+                return View(product);
+            }
+            catch {
+                return RedirectToAction("Index", "Home");
+            }
         }
 
-        // public async Task<IActionResult> Details(int? id) {
-        //     if (id == null) {
-        //         return NotFound();
-        //     }
+        public async Task<IActionResult> List(int catId, int page=1) {
+            try {
+                // Pagination
+                var pageSize = 10;
+                var category = await _context.Categories.FindAsync(catId);
+                var listProducts = _context.Products.AsNoTracking()
+                                                    .Where(p => p.CatId == catId)
+                                                    .OrderByDescending(p => p.ProductId);
 
-        //     var product = await _context.Products.FirstOrDefaultAsync(p => p.ProductId == id);
+                PagedList<Product> models = new PagedList<Product>(listProducts, page, pageSize);
 
-        //     if (product == null) {
-        //         return NotFound();
-        //     }
+                ViewBag.CurrentPage = page;
+                ViewBag.CurrentCat = category;
 
-        //     return View(product);
-        // }
+                return View(models);
+            }
+            catch {
+                return RedirectToAction("Index", "Home");
+            }
+        }
     }
 }
